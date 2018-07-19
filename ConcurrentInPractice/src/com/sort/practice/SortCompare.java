6c7a91c9d7f03e2cbb42bf2b7790e2a4ba2e1673
LiRui <lirui@banmatrip.com>
2018-07-19 11:49:42 +0800

    sortCompare

--- ConcurrentInPractice/src/com/sort/practice/SortCompare.java
+++ ConcurrentInPractice/src/com/sort/practice/SortCompare.java
@@ -17,6 +17,14 @@ public class SortCompare {
 			SelectionSort.sort(a);
 			long end = System.currentTimeMillis();
 			return end - start;
+		} else if (alg.equals("Shell")) {
+			ShellSort.sort(a);
+			long end = System.currentTimeMillis();
+			return end - start;
+		} else if (alg.equals("Merge")) {
+			MergeSort.sort(a);
+			long end = System.currentTimeMillis();
+			return end - start;
 		}
 		return 0;
 	}
@@ -34,11 +42,15 @@ public class SortCompare {
 	}
 	
 	public static void main(String[] args) {
-		long selectionSortTime = timeRandomInput("Selection", 10000, 1000);
-		long insertionSortTime = timeRandomInput("Insertion", 10000, 1000);
-		System.out.println("选择排序用时:" + selectionSortTime);
-		System.out.println("插入排序用时:" + insertionSortTime);
-		System.out.println("插入排序比选择排序快" + (selectionSortTime / insertionSortTime) + "倍");
+		//long selectionSortTime = timeRandomInput("Selection", 100000, 1);
+		//long insertionSortTime = timeRandomInput("Insertion", 100000, 1);
+		long shellSortTime = timeRandomInput("Shell", 10000000, 1);
+		long mergeSortTime = timeRandomInput("Merge", 10000000, 1);
+		//System.out.println("选择排序用时:" + selectionSortTime);
+		//System.out.println("插入排序用时:" + insertionSortTime);
+		System.out.println("希尔排序用时:" + shellSortTime);
+		System.out.println("归并排序用时:" + mergeSortTime);
+		//System.out.println("插入排序比选择排序快" + (selectionSortTime / insertionSortTime) + "倍");
 	}
 	
 }

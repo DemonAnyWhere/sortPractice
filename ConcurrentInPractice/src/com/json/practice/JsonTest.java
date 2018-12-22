ac39278639af9610d606e22cd520c826e4a4a87d
LiRui <lirui@banmatrip.com>
2018-12-22 19:48:15 +0800

    Revert "链表"
    This reverts commit b7b4c17ae02d3d56d52c9d3e394a9889fe26e33e.

--- ConcurrentInPractice/src/com/json/practice/JsonTest.java
+++ ConcurrentInPractice/src/com/json/practice/JsonTest.java
@@ -6,15 +6,22 @@ import org.junit.Test;
 
 public class JsonTest {
 
-	
-	
 	@Test
 	public void test() {
 		
 	}
 	
 	
+	class Person{
+		private List<Integer> receivers;
 
-	
-	
+		public List<Integer> getReceivers() {
+			return receivers;
+		}
+
+		public void setReceivers(List<Integer> receivers) {
+			this.receivers = receivers;
+		}
+		
+	}
 }
